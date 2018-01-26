e596b41dd8d07335eae01fb6b727f7d860a3cd43
josh16 <josh.randall16@gmail.com>
2018-01-26 18:53:08 -0500

    Import Sprites
    -Imported sprites, sliced them for correct size
    -Added Player sprite to Game
    -Created Player Controller

---
+++ Assets/__Scripts/PlayerController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LearnProgrammingAcademy.AstroAssault {
+
+    public class PlayerController : MonoBehaviour
+    {
+        #region == Constants ==
+        private const string HORIZONTAL = "Horizontal";
+        private const string VERTICAL = "Vertical";
+        #endregion
+
+        #region == fields ==
+        [SerializeField]
+        private float speed = 15;
+
+        private Rigidbody2D body;
+
+        #endregion
+
+        #region == Messages ==
+        private void Start()
+        {
+            body = GetComponent<Rigidbody2D>();
+
+        }
+
+        //This function is called every fixed frame
+        private void FixedUpdate()
+        {
+            float horizontalMovement = Input.GetAxis(HORIZONTAL);
+
+            body.velocity = new Vector2(horizontalMovement * speed, 0f);
+
+
+        }
+
+        #endregion
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
