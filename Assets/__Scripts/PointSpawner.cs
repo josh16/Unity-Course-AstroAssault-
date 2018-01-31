c16cf85827ae81c42248f764e7b24a343a1eef68
Joshua Randall <josh.randall16@gmail.com>
2018-01-31 00:40:06 -0500

    Defensive Programming
    -Created a check ‘if’ statement just in case if the
    “ENEMIES_PARENT_NAME” didn’t exist in the in the Unity  hierarchy and
    if it didn’t, a debug log would be displayed and a new gameObject will
    be created

--- Assets/__Scripts/PointSpawner.cs
+++ Assets/__Scripts/PointSpawner.cs
@@ -12,6 +12,7 @@ namespace LearnProgrammingAcademy.AstroAssault
 
         //Better to have a const name instead of hard coding a string
         private const string SPAWN_METHOD_NAME = "Spawn";
+        private const string ENEMIES_PARENT_NAME = "Enemies";
 
         // == fields == 
 
@@ -30,12 +31,25 @@ namespace LearnProgrammingAcademy.AstroAssault
 
 
         private IList<SpawnPoint> spawnPoints; // declared as interface so it will be converted to List
+        private GameObject enemiesParent;
+
 
         // == messages ==
         private void Start()
         {
             // This will find components with name "spawnPoint"
-            spawnPoints = GetComponentsInChildren<SpawnPoint>(); 
+            spawnPoints = GetComponentsInChildren<SpawnPoint>();
+            enemiesParent = GameObject.Find(ENEMIES_PARENT_NAME);
+
+            //Check to see if enemiesParent is null and if so, it will create a new object
+            //Always good to have a check to avoid errors and such
+            if(!enemiesParent)
+            {
+                Debug.Log($"{ENEMIES_PARENT_NAME} object not found, creating new object!");
+                enemiesParent = new GameObject(ENEMIES_PARENT_NAME); 
+            }
+
+
             SpawnRepeating();
         }
 
@@ -54,7 +68,8 @@ namespace LearnProgrammingAcademy.AstroAssault
             var randomIndex = Random.Range(0, spawnPoints.Count);
             var SpawnPoint = spawnPoints[randomIndex]; // The spawnpoints will be 
 
-            var enemy = Instantiate(enemyPrefab); // Instantiate the Enemy
+            var enemy = Instantiate(enemyPrefab,enemiesParent.transform); // Instantiate the Enemy
+                                                         // Parent of enemy is now "enemies parent"
 
             //Enemy will instantiate at the spawnpoints position
             enemy.transform.position = SpawnPoint.transform.position; 
