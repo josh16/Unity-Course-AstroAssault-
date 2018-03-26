e28ca4bba0b353a7f80710482707b89b055fb760
Joshua Randall <josh.randall16@gmail.com>
2018-03-26 14:39:56 -0400

    MainMenu Controller and Options Screen
    MainMenuController
     -Created a main menu controller that will have a function that
    controls the scene.
     - Added the scene manager name space to access the scene manager class
    and while using that class, we used the “LoadSceneASync” method to load
    the scene asynchronously in the background for better performance.
    
    SceneNames
     - Created a script that will have the scene names as a constant
    
    OptionsScene
      -Created a new scene that will be the options screen

---
+++ Assets/__Scripts/Scene/MainMenuSceneController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using LearnProgrammingAcademy.AstroAssault.Utils;
+
+
+namespace LearnProgrammingAcademy.AstroAssault.Scene{
+
+    public class MainMenuSceneController : MonoBehaviour{
+
+        // == OnClick Events == 
+        public void PlayOnClick(){
+            SceneManager.LoadSceneAsync(SceneNames.LEVEL);
+        }
+
+    }
+}
---
+++ Assets/__Scripts/UI/SceneNames.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LearnProgrammingAcademy.AstroAssault.Utils{
+
+    public class SceneNames : MonoBehaviour{
+
+        // == constants == 
+        public const string LEVEL = "Level";
+
+
+    }
+}
